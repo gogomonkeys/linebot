f43a2e56cd742b9940e593bf5769a0c829572843
{ggmk} <{gogomonkey780918@gmail.com}>
2024-10-02 17:25:16 +0800

    relay muti type

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, abort
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
-from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
-from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage, ImageMessage, LocationMessage, StickerMessage
+from linebot.v3.webhooks import MessageEvent, TextMessageContent, ImageMessageContent, LocationMessageContent, StickerMessageContent
 import json
 
 app = Flask(__name__)
@@ -32,11 +32,37 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessageContent)
+@handler.add(MessageEvent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages=[TextMessage(text=event.message.text)]))
+
+        # 根據接收到的訊息類型進行對應回覆
+        if isinstance(event.message, TextMessageContent):
+            reply_message = TextMessage(text=event.message.text)
+        elif isinstance(event.message, ImageMessageContent):
+            reply_message = ImageMessage(
+                original_content_url=event.message.content_provider.original_content_url,
+                preview_image_url=event.message.content_provider.preview_image_url
+            )
+        elif isinstance(event.message, LocationMessageContent):
+            reply_message = LocationMessage(
+                title=event.message.title,
+                address=event.message.address,
+                latitude=event.message.latitude,
+                longitude=event.message.longitude
+            )
+        elif isinstance(event.message, StickerMessageContent):
+            reply_message = StickerMessage(
+                package_id=event.message.package_id,
+                sticker_id=event.message.sticker_id
+            )
+        else:
+            reply_message = TextMessage(text="不支援的訊息類型")
+
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(reply_token=event.reply_token, messages=[reply_message])
+        )
 
 
 
